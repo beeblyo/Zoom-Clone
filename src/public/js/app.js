743b63474bcddcd7891d0c08b0dd8e03963ab1aa
beebly <you@example.com>
2023-01-15 23:36:50 +0900

    getting client input value to server to chatting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,18 @@
-alert('hey');
-// const test = document.querySelector(body);
-// test.addEventListener("click",()=>{console.log("hey??")});
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+// Create WebSocket connection.
+const socket = new WebSocket(`ws://${window.location.host}`);
+socket.addEventListener("open",()=>{console.log("connected to server!")});
+socket.addEventListener("message",(message)=>{console.log(`i've got this message : ${message.data}`)});
+socket.addEventListener("close",()=>{console.log("disconnected from server :(")});
+
+
+function handleSubmit(evt){
+    evt.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit",handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import http from "http";
+import { WebSocketServer } from "ws";
 
 const workingDirectory = path.resolve();
 const app = express();
@@ -9,8 +11,23 @@ app.set("views",workingDirectory +"/src/views");
 
 app.use('/public', express.static(path.join(workingDirectory, 'src/public')));
 
-app.get('/',(req,res)=>{
-    res.render('home')
+app.get('/',(req,res)=>{res.render('home')});
+app.get("/*",(req,res)=>{res.redirect("/")});
+
+// http와 webSocket 서버를 같은 port로 listening 해준다
+// function handleConnection(socket) {console.log(socket);
+// 각각의 브라우저 sockets 을 서버에서 받는다
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
+const sockets = [];
+
+wss.on("connection", (socket)=>{
+    sockets.push(socket);
+
+    console.log("connected to browser");
+    socket.on("message",(message)=>{sockets.forEach((broswerSocket)=>{broswerSocket.send(message.toString)})})
+    socket.on("close",()=>{console.log("broswer is closed")})
 });
 
-app.listen(3000);
+server.listen(3000);
