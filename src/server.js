90d27a285446a90d309da1c703eaaf6821db285b
beebly <qw1460@naver.com>
2023-01-17 23:28:00 +0900

    connected with socket.io on client end

--- src/server.js
+++ src/server.js
@@ -14,9 +14,11 @@ app.use('/public', express.static(path.join(workingDirectory, 'src/public')));
 app.get('/',(req,res)=>{res.render('home')});
 app.get("/*",(req,res)=>{res.redirect("/")});
 
-
 const httpServer = http.createServer(app);
 const ioServer =  new Server(httpServer);
 
+ioServer.on("connection",socket=>{
+    console.log(socket)
+});
 
 httpServer.listen(3000);
